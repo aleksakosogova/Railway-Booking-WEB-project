3ed0dbceff6cadf89a893c913f096f61116ea2ee
Kosogova Sasha <aleksa.kosogova@gmail.com>
2026-04-29 21:12:12 +0300

    feat: create mock data for train schedule

---
+++ src/data/trains.js
@@ -0,0 +1,4 @@
+export const trains = [
+  { id: 1, number: "705К", from: "Київ", to: "Львів", departureDate: "2026-05-15", departureTime: "06:00", duration: "6г 20хв" },
+  { id: 2, number: "091К", from: "Київ", to: "Одеса", departureDate: "2026-05-15", departureTime: "21:15", duration: "9г 40хв" }
+];
