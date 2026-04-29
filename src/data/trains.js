229f3b097022f5be10bf735cc7d2b7d127e491ca
Kosogova Sasha <aleksa.kosogova@gmail.com>
2026-04-29 21:29:49 +0300

    feat: expand train schedule to 6 routes

--- src/data/trains.js
+++ src/data/trains.js
@@ -1,4 +1,8 @@
 export const trains = [
-  { id: 1, number: "705К", from: "Київ", to: "Львів", departureDate: "2026-05-15", departureTime: "06:00", duration: "6г 20хв" },
-  { id: 2, number: "091К", from: "Київ", to: "Одеса", departureDate: "2026-05-15", departureTime: "21:15", duration: "9г 40хв" }
-];
+    { id: 1, number: "705К", from: "Київ", to: "Львів", departureDate: "2026-05-15", departureTime: "06:00", duration: "6г 20хв" },
+    { id: 2, number: "091К", from: "Київ", to: "Одеса", departureDate: "2026-05-15", departureTime: "21:15", duration: "9г 40хв" },
+    { id: 3, number: "001І", from: "Львів", to: "Дніпро", departureDate: "2026-05-16", departureTime: "19:00", duration: "12г 30хв" },
+    { id: 4, number: "743Л", from: "Львів", to: "Київ", departureDate: "2026-05-17", departureTime: "05:50", duration: "6г 10хв" },
+    { id: 5, number: "012П", from: "Одеса", to: "Харків", departureDate: "2026-05-17", departureTime: "18:20", duration: "11г 15хв" },
+    { id: 6, number: "043К", from: "Київ", to: "Івано-Франківськ", departureDate: "2026-05-18", departureTime: "20:00", duration: "9г 50хв" }
+  ];
